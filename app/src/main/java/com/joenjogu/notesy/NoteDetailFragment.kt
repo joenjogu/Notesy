5fb31ae393236843486a4db3cba48efd6c8f3050
Joe Njogu <joenjogu97@gmail.com>
2021-02-17 13:27:15 +0300

    ui layouts done

--- app/src/main/java/com/joenjogu/notesy/NoteDetailFragment.kt
+++ app/src/main/java/com/joenjogu/notesy/NoteDetailFragment.kt
@@ -1,7 +1,38 @@
 package com.joenjogu.notesy
 
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.joenjogu.notesy.databinding.FragmentNoteDetailBinding
 
 class NoteDetailFragment : Fragment() {
+    private lateinit var binding : FragmentNoteDetailBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_note_detail, container, false)
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_note_detail, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.action_delete) {
+            // handle delete click
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
 
 }
