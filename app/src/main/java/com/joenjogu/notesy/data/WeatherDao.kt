63dc96369a7ad8af7f4ede2eee9b0a39f3efdbca
Joe Njogu <joenjogu97@gmail.com>
2021-02-27 17:38:05 +0300

    api repository tweaks

--- app/src/main/java/com/joenjogu/notesy/data/WeatherDao.kt
+++ app/src/main/java/com/joenjogu/notesy/data/WeatherDao.kt
@@ -4,12 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.joenjogu.notesy.WeatherResponse
 
 @Dao
 interface WeatherDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertWeather()
+    fun insertWeather(weatherResponse: WeatherResponse)
 
     @Query("")
-    fun getWeather()
+    fun getWeather(): WeatherResponse
 }
--- app/src/main/java/com/joenjogu/notesy/data/WeatherRepository.kt
+++ app/src/main/java/com/joenjogu/notesy/data/WeatherRepository.kt
@@ -1,6 +1,7 @@
 package com.joenjogu.notesy.data
 
 import android.accounts.NetworkErrorException
+import android.util.Log
 import com.joenjogu.notesy.API_KEY
 import com.joenjogu.notesy.NAIROBI_CITY_ID
 import java.io.IOException
@@ -14,7 +15,17 @@ class WeatherRepository(private val weatherService: WeatherService, private val 
             val weatherForecast = weatherService.getThreeDayForecast(NAIROBI_CITY_ID, 3, API_KEY)
             dao.insertWeather(weatherForecast)
         }
-        catch (exception : IOException){}
-        catch (exception: NetworkErrorException) {}
+        catch (exception : IOException){
+            Log.d(TAG, "getForecast: $exception")
+            throw exception
+        }
+        catch (exception: NetworkErrorException) {
+            Log.d(TAG, "getForecast: $exception")
+            throw exception
+        }
+    }
+
+    companion object {
+        val TAG: String = this::class.java.name
     }
 }
